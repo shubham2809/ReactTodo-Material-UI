695944e3fb50fb87f2f7a4426ab08a79f009e662
Shubham Deodia <shubham.deodia@amdocs.com>
2019-07-26 10:20:35 +0300

    Updates

---
+++ precache-manifest.c8eb126bd06a3cd89d1f30b23958af91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcd17c2cc2da98a453aceff7cebc548",
+    "url": "/ReactTodo-Material-UI/index.html"
+  },
+  {
+    "revision": "dfd1d9f8761a41a79c81",
+    "url": "/ReactTodo-Material-UI/static/css/main.20b02462.chunk.css"
+  },
+  {
+    "revision": "d2f273577932d1d9c0de",
+    "url": "/ReactTodo-Material-UI/static/js/2.b7321446.chunk.js"
+  },
+  {
+    "revision": "dfd1d9f8761a41a79c81",
+    "url": "/ReactTodo-Material-UI/static/js/main.faedc845.chunk.js"
+  },
+  {
+    "revision": "7db1b0fecb36f663e0ef",
+    "url": "/ReactTodo-Material-UI/static/js/runtime~main.0830fb4f.js"
+  },
+  {
+    "revision": "f80573152b6c2a74dffe779b9fbd362d",
+    "url": "/ReactTodo-Material-UI/static/media/bg-image.f8057315.png"
+  }
+]);
